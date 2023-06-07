a86771d4d221d545e94ca97dcddd22da9de1b55b
Mikołaj Lubiak <lubiak@proton.me>
2023-06-07 21:10:48 +0000

    First version that works

--- cogs/register.py
+++ cogs/register.py
@@ -6,48 +6,86 @@ import os
 import mariadb
 import sys
 
-try:
-	conn = mariadb.connect (
-	user="mariadb",
-	password="passwd",
-	host="localhost",
-	port=3306,
-	database="BusinessBot"
-	)
-except mariadb.Error as e:
-	print(f"Error connecting to MariaDB Platform: {e}")
-	sys.exit(1)
 
-cur = conn.cursor()
+def check_user_exists(discordID: int):
+    conn = mariadb.connect (
+        user="root",
+        password="passwd",
+        host="localhost",
+        port=3306,
+        database="bot"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(discord_id) FROM users WHERE discord_id = ? LIMIT 1", (discordID,))
+    res = cur.fetchone()
+    conn.close()
+    return res[0]
 
-def check_user_exists(id : int):
-	cur.execute("SELECT COUNT(discord_id) FROM users WHERE discord_id = ? LIMIT 1", (id,))
-	return int(cur.fetchone())
-
+def check_company_exists(name):
+    conn = mariadb.connect (
+        user="root",
+        password="passwd",
+        host="localhost",
+        port=3306,
+        database="bot"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(name) FROM companies WHERE name = ? LIMIT 1", (name,))
+    res = cur.fetchone()
+    conn.close()
+    return res[0]
 
 class Register(commands.Cog):
-	def __init__(self,bot):
-		self.bot = bot
+    def __init__(self,bot):
+        self.bot = bot
 
-	@commands.Cog.listener()
-	async def on_ready(self):
-		print('Cog: Register is on')
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Cog: Register is on')
 
-	@commands.command()
-	async def register(self, ctx, name):
-		if not check_user_exists():
-            discordID = ctx.author.id
-			cur.execute("INSERT INTO users (discord_id) VALUES (?);", (discordID,))
-			cur.execute("SELECT (user_id) FROM users WHERE discord_id = ?", (discordID,))
-            userID = cur.user_id
-			cur.execute("INSERT INTO companies (name, user_id) VALUES (?, ?)", (name, userID))
-			embed=nextcord.Embed(title="Witamy w świecie biznesu, ", description="Użyj komendy !tutorial", color=0x44ff00)
-			await ctx.send(embed=embed)
-		else:
-			embed=nextcord.Embed(title="Posiadasz już konto", color=0xff0000)
-			await ctx.send(embed=embed)
+    @commands.command()
+    async def register(self, ctx):
+        conn = mariadb.connect (
+            user="root",
+            password="passwd",
+            host="localhost",
+            port=3306,
+            database="bot"
+        )
+        cur = conn.cursor()
+        discordID = ctx.author.id
+        if not check_user_exists(discordID):
+            cur.execute("INSERT INTO users (discord_id) VALUES (?);", (discordID,))
+            conn.commit()
+            embed=nextcord.Embed(title="Witamy w świecie biznesu, ", description="Użyj komendy !tutorial", color=0x44ff00)
+            await ctx.send(embed=embed)
+        else:
+            embed=nextcord.Embed(title="Posiadasz już konto", color=0xff0000)
+            await ctx.send(embed=embed)
+        conn.close()
+
+    @commands.command()
+    async def company(self, ctx, name):
+        conn = mariadb.connect (
+            user="root",
+            password="passwd",
+            host="localhost",
+            port=3306,
+            database="bot"
+        )
+        cur = conn.cursor()
+        discordID = ctx.author.id
+        if not check_company_exists(name):
+            cur.execute("SELECT (user_id) FROM users WHERE discord_id = ?", (discordID,))
+            userID = cur.fetchone()[0]
+            cur.execute("INSERT INTO companies (name, user_id) VALUES (?, ?)", (name, userID))
+            conn.commit()
+            embed=nextcord.Embed(title="Założono firmę, ", description=f"Nazwa firmy: {name}", color=0x44ff00)
+            await ctx.send(embed=embed)
+        else:
+            embed=nextcord.Embed(title="Firma z tą nazwą już istnieje", color=0xff0000)
+            await ctx.send(embed=embed)
+        conn.close()
 
 def setup(bot):
-	bot.add_cog(Register(bot))
-
-conn.close()
+    bot.add_cog(Register(bot))
